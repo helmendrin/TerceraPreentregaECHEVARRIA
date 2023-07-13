a5b6cb0b3ffcd733e0023a792bf024afeca45653
Helen Echevarria <eche.helen@gmail.com>
2023-07-13 11:51:10 -0300

    add: modelo Estudiante, Profesor, Curso

--- project/apps/CORE/models.py
+++ project/apps/CORE/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    
+
+    def __str__(self):
+        return self.nombre
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    especialidad = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.nombre
+    
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
