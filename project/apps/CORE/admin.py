95ea2fbe832655b8a59167930b0f9915d8184165
Helen Echevarria <eche.helen@gmail.com>
2023-07-13 12:04:03 -0300

    add: modelos registrados

--- project/apps/CORE/admin.py
+++ project/apps/CORE/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register (models.Estudiante)
+admin.site.register (models.Profesor)
+admin.site.register (models.Curso)
